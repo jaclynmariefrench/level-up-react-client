76b11cde7e32f76d5d716832158ba6c625251d08
Jaclyn French <jaclynmariefrench@gmail.com>
2021-08-13 11:50:31 -0500

    finished ch13

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,6 +6,8 @@ import { GameList } from "./game/GameList.js"
 import { GameProvider } from "./game/GameProvider.js"
 import { GameForm } from "./game/GameForm.js"
 import { EventForm } from "./game/EventForm.js"
+import { ProfileProvider } from "./auth/ProfileProvider.js"
+import { Profile } from "./auth/Profile.js"
 
 export const ApplicationViews = () => {
     return <>
@@ -13,25 +15,27 @@ export const ApplicationViews = () => {
             margin: "5rem 2rem",
             lineHeight: "1.75rem"
         }}>
+        <EventProvider>
             <GameProvider>
-                <Route exact path="/games">
-                    <GameList />
-                </Route>
-                <Route exact path="/games/new">
-                    <GameForm />
-                </Route>
-
-            </GameProvider>
-            <EventProvider>
-                <GameProvider>
+                <ProfileProvider>
+                    <Route exact path="/games">
+                        <GameList />
+                    </Route>
+                    <Route exact path="/games/new">
+                        <GameForm />
+                    </Route>
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
                     <Route exact path="/events">
                         <EventList />
                     </Route>
                     <Route exact path="/events/new">
                         <EventForm/>
                     </Route>
-                </GameProvider>
-            </EventProvider>
+                </ProfileProvider>
+        </GameProvider>
+        </EventProvider>
 
         </main>
     </>
